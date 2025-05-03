041efd5fe3364170d1544106b77d715e5ba1b61b
dana <danaputra1001@gmail.com>
2025-05-03 20:20:25 +0700

    first commit

---
+++ src/Pages/Status1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Status1() {
+  return (
+    <div>
+      <h1 className="font-bold text-9xl ">test</h1>
+    </div>
+    
+  )
+}
+
+export default Status1
---
+++ src/Pages/status2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sttaus2() {
+  return (
+    <div>Sttaus2</div>
+  )
+}
+
+export default Sttaus2
